423ffd9f23359c654823e210325a879f553a8673
nic8854 <40262536+nic8854@users.noreply.github.com>
2025-11-23 16:57:00 +0100

    create structure to implement algorhythm

--- src/main.c
+++ src/main.c
@@ -17,17 +17,49 @@
 
 #define UPDATETIME_MS 100
 
-void templateTask(void* param) {
-    //Init stuff here
-    vTaskDelay(100);
+typedef struct {
+    double     piValue;
+    TickType_t tickCount;
+} piResult_t;
+
+EventGroupHandle_t piCalcEventGroup;
+QueueHandle_t leibnizQueue;
+
+void controlTask(void* param) {
+    piResult_t lebnizResult;
+    uint16_t xpos = 20;
+	uint16_t ypos = 50;
+	char displayPi[24];
+    char displayTicks[24];
+    char displayTime[24];
+	uint16_t color = GREEN;
     for(;;) {
-        // task main loop
-        if(button_get_state(SW0, true) == SHORT_PRESSED) {
-            led_toggle(LED0);
+        if(xQueueReceive(leibnizQueue, &lebnizResult, portMAX_DELAY) == pdTRUE) {
+            xQueueReset(leibnizQueue);
+            sprintf((char*)displayPi, "Pi = %f", lebnizResult.piValue);
+            sprintf((char*)displayTicks, "Ticks = %d", (int)lebnizResult.tickCount);
+            sprintf((char*)displayTime, "Time = %.3fs", ((float)(lebnizResult.tickCount * portTICK_PERIOD_MS)) / 1000);
+            lcdFillScreen(BLACK);
+            lcdDrawString(fx32L, xpos, ypos, &displayPi[0], color);
+            lcdDrawString(fx32L, xpos, ypos+50, &displayTicks[0], color);
+            lcdDrawString(fx32L, xpos, ypos+100, &displayTime[0], color);
+            lcdUpdateVScreen();
         }
-        led_toggle(LED7);
-        // delay
-        vTaskDelay(UPDATETIME_MS/portTICK_PERIOD_MS);
+        vTaskDelay(50/portTICK_PERIOD_MS);
+    }
+}
+
+void leibnizTask(void* param) {
+    piResult_t piResult;
+    piResult.piValue = 0;
+    vTaskDelay(100);
+
+    for(;;) {
+        piResult.tickCount = xTaskGetTickCount();
+        piResult.piValue++;
+        xQueueSendToFront(leibnizQueue, &piResult, 0);
+
+        taskYIELD();  // Yield to allow IDLE task brief execution
     }
 }
 
@@ -35,16 +67,13 @@ void app_main()
 {
     //Initialize Eduboard2 BSP
     eduboard2_init();
+
+    piCalcEventGroup = xEventGroupCreate();
+    leibnizQueue = xQueueCreate(100, sizeof(piResult_t));
     
     //Create templateTask
-    xTaskCreate(templateTask,   //Subroutine
-                "testTask",     //Name
-                2*2048,         //Stacksize
-                NULL,           //Parameters
-                10,             //Priority
-                NULL);          //Taskhandle
-    for(;;) {
-        vTaskDelay(2000/portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "Hello Eduboard");
-    }
+    xTaskCreatePinnedToCore(controlTask, "controlTask", 2*2048, NULL, 10, NULL, 0);
+    xTaskCreatePinnedToCore(leibnizTask, "leibnizTask", 2*2048, NULL, 1, NULL, 1);
+
+    return;
 }
